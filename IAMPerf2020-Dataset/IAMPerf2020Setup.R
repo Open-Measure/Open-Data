232d560e75840cbc63e8a25fb048dbe066e80d53
DM3D <david.doret@me.com>
2020-07-05 16:13:34 +0200

    Update IAMPerf2020Setup.R

--- IAMPerf2020-Dataset/IAMPerf2020Setup.R
+++ IAMPerf2020-Dataset/IAMPerf2020Setup.R
@@ -20,32 +20,32 @@ iamperf2020_q23_goals_url = paste0(iamperf2020_data_url, "IAMPerf2020Q23Goals.cs
 iamperf2020_q23_priorities_url = paste0(iamperf2020_data_url, "IAMPerf2020Q23Priorities.csv");
 
 # Load the data
-iamperf2020_data <- read.csv (text = RCurl::getURL(iamperf2020_survey_url));
+iamperf2020_survey <- read.csv (text = RCurl::getURL(iamperf2020_survey_url));
 iamperf2020_q20_domains <- read.csv (text = RCurl::getURL(iamperf2020_q20_domains_url));
 iamperf2020_q20_team_dedication <- read.csv (text = RCurl::getURL(iamperf2020_q20_team_dedication_url))
 iamperf2020_q23_goals <- read.csv (text = RCurl::getURL(iamperf2020_q23_goals_url));
 iamperf2020_q23_priorities <- read.csv (text = RCurl::getURL(iamperf2020_q23_priorities_url))
 
 # Q20: Apply nicely labeled and properly ordered factors.
-iamperf2020_data$Q20R1 = factor(iamperf2020_data$Q20R1, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q20R2 = factor(iamperf2020_data$Q20R2, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q20R3 = factor(iamperf2020_data$Q20R3, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q20R4 = factor(iamperf2020_data$Q20R4, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q20R5 = factor(iamperf2020_data$Q20R5, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q20R6 = factor(iamperf2020_data$Q20R6, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R1 = factor(iamperf2020_survey$Q20R1, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R2 = factor(iamperf2020_survey$Q20R2, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R3 = factor(iamperf2020_survey$Q20R3, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R4 = factor(iamperf2020_survey$Q20R4, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R5 = factor(iamperf2020_survey$Q20R5, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q20R6 = factor(iamperf2020_survey$Q20R6, levels = iamperf2020_q20_team_dedication$X, labels = iamperf2020_q20_team_dedication$Title, ordered = TRUE, exclude = NA);
 
 # Q23: Apply nicely labeled and properly ordered factors.
-iamperf2020_data$Q23R1 = factor(iamperf2020_data$Q23R1, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R2 = factor(iamperf2020_data$Q23R2, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R3 = factor(iamperf2020_data$Q23R3, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R4 = factor(iamperf2020_data$Q23R4, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R5 = factor(iamperf2020_data$Q23R5, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R6 = factor(iamperf2020_data$Q23R6, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R7 = factor(iamperf2020_data$Q23R7, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R8 = factor(iamperf2020_data$Q23R8, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R9 = factor(iamperf2020_data$Q23R9, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R10 = factor(iamperf2020_data$Q23R10, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
-iamperf2020_data$Q23R11 = factor(iamperf2020_data$Q23R11, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R1 = factor(iamperf2020_survey$Q23R1, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R2 = factor(iamperf2020_survey$Q23R2, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R3 = factor(iamperf2020_survey$Q23R3, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R4 = factor(iamperf2020_survey$Q23R4, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R5 = factor(iamperf2020_survey$Q23R5, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R6 = factor(iamperf2020_survey$Q23R6, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R7 = factor(iamperf2020_survey$Q23R7, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R8 = factor(iamperf2020_survey$Q23R8, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R9 = factor(iamperf2020_survey$Q23R9, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R10 = factor(iamperf2020_survey$Q23R10, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
+iamperf2020_survey$Q23R11 = factor(iamperf2020_survey$Q23R11, levels = iamperf2020_q23_priorities$X, labels = iamperf2020_q23_priorities$Title, ordered = TRUE, exclude = NA);
 
 print("IAMPerf2020 environment loaded.")
 
